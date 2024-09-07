fd1387efc127a705c20c35ff87505db1069a4abc
Nikita Tikhomirov <tihomnik@yandex.ru>
2024-09-07 18:44:11 +0300

    Moved the loop out of the condition block

--- Lesson_1/DopZadacha.java
+++ Lesson_1/DopZadacha.java
@@ -1,19 +1,21 @@
 public class DopZadacha {
     public LinkedList sumList(LinkedList first, LinkedList second) {
         LinkedList resultList = new LinkedList();
-        if (first.count() == second.count()) {
-            Node firstListNode = first.head;
-            Node secondListNode = second.head;
-
-            while (firstListNode != null) {
-                int sum = firstListNode.value + secondListNode.value;
-                resultList.addInTail(new Node(sum));
-
-                firstListNode = firstListNode.next;
-                secondListNode = secondListNode.next;
-            }
+        if (first.count() != second.count()) {
+            return resultList;
         }
+        
+        Node firstListNode = first.head;
+        Node secondListNode = second.head;
 
+        while (firstListNode != null) {
+            int sum = firstListNode.value + secondListNode.value;
+            resultList.addInTail(new Node(sum));
+
+            firstListNode = firstListNode.next;
+            secondListNode = secondListNode.next;
+        }
+            
         return resultList;
     }
 }
