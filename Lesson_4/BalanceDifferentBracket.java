600999a9514dfaecd8248e31195791abbcabdae0
Nikita Tikhomirov <tihomnik@yandex.ru>
2024-09-23 15:45:22 +0300

    Optimized PostfixNotation

--- Lesson_4/BalanceDifferentBracket.java
+++ Lesson_4/BalanceDifferentBracket.java
@@ -4,15 +4,15 @@ public class BalanceDifferentBracket {
     public boolean isBalanced(String str) {
         Stack<Character> bracketStack = new Stack<>();
         byte[] byteArray = str.getBytes();
-        Map<Character, Character> map = Map.of(')', '(', ']', '[', '}', '{');
+        Map<Character, Character> map = Map.of('(', ')', '[', ']', '{', '}');
 
         for (byte b: byteArray) {
-            if (map.containsValue((char) b)) {
-                bracketStack.push((char) b);
+            if (map.containsKey((char) b)) {
+                bracketStack.push(map.get((char) b));
                 continue;
             }
 
-            if (bracketStack.peek() == null || (bracketStack.peek() != map.get((char) b))) {
+            if (bracketStack.peek() == null || (bracketStack.peek() != (char) b)) {
                 return false;
             }
 
