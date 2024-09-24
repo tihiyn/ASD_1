5ad7e64f5b0488c93a38aef17ae72c9342fefae3
Nikita Tikhomirov <tihomnik@yandex.ru>
2024-09-24 10:49:28 +0300

    Used declarative style

--- Lesson_4/PostfixNotation.java
+++ Lesson_4/PostfixNotation.java
@@ -1,26 +1,30 @@
 public class PostfixNotation {
+    private final Map<Character, BiFunction<BigDecimal, BigDecimal, BigDecimal>> operationMap = new HashMap<>();
+    
+    public PostfixNotation() {
+        operationMap.put('+', BigDecimal::add);
+        operationMap.put('*', BigDecimal::multiply);
+        operationMap.put('-', BigDecimal::subtract);
+        operationMap.put('/', (x, y) -> {
+            if (y.equals(BigDecimal.ZERO)) {
+                throw new ArithmeticException("Division by zero");
+            }
+            return x.divide(y, 2, RoundingMode.HALF_UP);
+        });
+        operationMap.put('=', (x, y) -> x);
+    } 
+    
     public BigDecimal compute(Stack<Character> input) {
         Stack<BigDecimal> result = new Stack<>();
         while (input.size() > 0) {
             char symbol = input.pop();
-            if (Character.isDigit(symbol)) {
-                result.push(BigDecimal.valueOf(Character.getNumericValue(symbol)));
-                continue;
-            }
 
-            BigDecimal a = result.pop();
-            BigDecimal b = result.pop();
-            switch (symbol) {
-                case '+' -> result.push(a.add(b));
-                case '*' -> result.push(a.multiply(b));
-                case '-' -> result.push(a.subtract(b));
-                case '/' -> {
-                    if (b.equals(BigDecimal.ZERO)) {
-                        throw new ArithmeticException("Division by zero");
-                    }
-                    result.push(a.divide(b, 2, RoundingMode.HALF_UP));
-                }
-                case '=' -> {return a;}
+            if (operationMap.containsKey(symbol)) {
+                BigDecimal a = result.pop();
+                BigDecimal b = result.pop();
+                result.push(operationMap.get(symbol).apply(a, b));
+            } else {
+                result.push(BigDecimal.valueOf(Character.getNumericValue(symbol)));
             }
         }
 
