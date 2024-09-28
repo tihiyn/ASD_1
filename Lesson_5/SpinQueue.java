06a86fd026e79712d8cba201fc72b6517427b4af
Nikita Tikhomirov <tihomnik@yandex.ru>
2024-09-28 13:01:19 +0300

    Removed local var

--- Lesson_5/SpinQueue.java
+++ Lesson_5/SpinQueue.java
@@ -1,8 +1,7 @@
 public class SpinQueue<T> {
     public void spin(Queue<T> queue, int N) {
         for (int i = 0; i < N; i++) {
-            T e = queue.dequeue();
-            queue.enqueue(e);
+            queue.enqueue(queue.dequeue());
         }
     }
 }
