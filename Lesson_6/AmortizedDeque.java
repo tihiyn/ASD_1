49b5c9fb476ee883ede10203932912c11bc01dac
Nikita Tikhomirov <tihomnik@yandex.ru>
2024-10-04 13:54:57 +0300

    Divided the logic of deque and dynamic array

--- Lesson_6/AmortizedDeque.java
+++ Lesson_6/AmortizedDeque.java
@@ -1,31 +1,26 @@
 import java.lang.reflect.Array;
 
 public class AmortizedDeque<T> {
-    private T[] storage;
-    private Class clazz;
+    private DynamicArray<T> storage;
     private int startPointer;
     private int endPointer;
-    private int capacity;
     private int size;
 
-    public AmortizedDeque(Class clazz) {
-        capacity = 1;
-        storage = (T[]) Array.newInstance(clazz, capacity);
-        this.clazz = clazz;
+    public AmortizedDeque(Class<T> clazz) {
+        storage = new DynamicArray<>(clazz, 1);
         startPointer = 0;
         endPointer = 0;
         size = 0;
     }
 
     private void resize() {
-        T[] oldDeque = storage;
-        capacity *= 2;
-        storage = (T[]) Array.newInstance(clazz, capacity);
+        DynamicArray<T> newStorage = new DynamicArray<>(storage.getTypeOfElements(), storage.getCapacity() * 2);
 
         for (int i = 0; i < size; i++) {
-            storage[i] = oldDeque[(startPointer + i) % size];
+            newStorage.setItem(i, storage.getItem((startPointer + i) % storage.getCapacity()));
         }
 
+        storage = newStorage;
         startPointer = 0;
         endPointer = size;
     }
@@ -36,7 +31,7 @@ public class AmortizedDeque<T> {
         }
 
         startPointer = movePointerLeftInCycle(startPointer);
-        storage[startPointer] = item;
+        storage.setItem(startPointer, item);
         size++;
     }
 
@@ -45,7 +40,7 @@ public class AmortizedDeque<T> {
             resize();
         }
 
-        storage[endPointer] = item;
+        storage.setItem(endPointer, item);
         endPointer = movePointerRightInCycle(endPointer);
         size++;
     }
@@ -57,7 +52,7 @@ public class AmortizedDeque<T> {
 
         startPointer = movePointerRightInCycle(startPointer);
         size--;
-        return storage[movePointerLeftInCycle(startPointer)];
+        return storage.getItem(movePointerLeftInCycle(startPointer));
     }
 
     public T removeTail() {
@@ -67,23 +62,55 @@ public class AmortizedDeque<T> {
 
         endPointer = movePointerLeftInCycle(endPointer);
         size--;
-        return storage[endPointer];
+        return storage.getItem(endPointer);
     }
 
     private int movePointerLeftInCycle(int pointer) {
-        return (pointer - 1 + capacity) % capacity;
+        return (pointer - 1 + storage.getCapacity()) % storage.getCapacity();
     }
-    
+
     private int movePointerRightInCycle(int pointer) {
-        return (pointer + 1) % capacity;
+        return (pointer + 1) % storage.getCapacity();
     }
 
     public boolean isFull() {
-        return size == capacity;
+        return size == storage.getCapacity();
     }
 
     public int size() {
         return size;
+    }
+}
+
+class DynamicArray<T> {
+    private T[] storage;
+    private Class<T> clazz;
+    private int capacity;
+
+    public DynamicArray(Class<T> clazz, int initialCapacity) {
+        this.clazz = clazz;
+        this.capacity = initialCapacity;
+        storage = (T[]) Array.newInstance(clazz, initialCapacity);
+    }
+
+    public DynamicArray<T> getExtendedArray(int newCapacity) {
+        return new DynamicArray<>(clazz, newCapacity);
+    }
+
+    public T getItem(int index) {
+        return storage[index];
+    }
+
+    public void setItem(int index, T value) {
+        storage[index] = value;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public Class<T> getTypeOfElements() {
+        return clazz;
     }
 }
 
