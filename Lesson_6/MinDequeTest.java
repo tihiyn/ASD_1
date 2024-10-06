15a5593c37af7467eafa1db7acfb15444e8350b3
Nikita Tikhomirov <tihomnik@yandex.ru>
2024-10-06 16:37:26 +0300

    Another test

--- Lesson_6/MinDequeTest.java
+++ Lesson_6/MinDequeTest.java
@@ -8,22 +8,18 @@ class MinDequeTest {
         MinDeque<Integer> deque = new MinDeque<>();
 
         deque.addTail(3);
-        deque.addTail(4);
         deque.addTail(5);
-        deque.addTail(6);
-        deque.addTail(7);
-
+        deque.addTail(4);
         assertEquals(3, deque.getMin());
 
-        deque.removeFront();
-        deque.removeFront();
+        deque.addTail(2);
+        assertEquals(2, deque.getMin());
 
-        assertEquals(5, deque.getMin());
-
-        deque.removeTail();
-        deque.removeTail();
-
-        assertEquals(5, deque.getMin());
+        assertEquals(2, deque.minDeque.removeTail());
+        assertEquals(3, deque.minDeque.removeTail());
+        assertEquals(4, deque.minDeque.removeTail());
+        assertEquals(5, deque.minDeque.removeTail());
+        assertNull(deque.minDeque.removeTail());
     }
 }
 
