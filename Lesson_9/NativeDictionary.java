f5bf6dfb5a10604e4d0d0c28644346789aad1f38
Nikita Tikhomirov <tihomnik@yandex.ru>
2024-10-10 00:29:01 +0300

    Lesson_9

---
+++ Lesson_9/NativeDictionary.java
@@ -0,0 +1,81 @@
+import java.lang.reflect.Array;
+
+class NativeDictionary<T>
+{
+    public int size;
+    public String [] slots;
+    public T [] values;
+    public int step;
+
+    public NativeDictionary(int sz, Class clazz)
+    {
+        size = sz;
+        slots = new String[size];
+        values = (T[]) Array.newInstance(clazz, this.size);
+        step = 3;
+    }
+
+    public int hashFun(String key)
+    {
+        int hash = 0;
+
+        for (byte b: key.getBytes()) {
+            hash = 31 * hash + (b & 0xff);
+        }
+
+        return Math.abs(hash) % size;
+    }
+
+    public boolean isKey(String key)
+    {
+        int hashIndex = hashFun(key);
+
+        for (; slots[hashIndex] != null; hashIndex = (hashIndex + step) % size) {
+            if (slots[hashIndex].equals(key)) {
+                return true;
+            }
+
+            if ((hashIndex + step) % size == hashIndex) {
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+    public void put(String key, T value)
+    {
+        int hashIndex = hashFun(key);
+        int index = hashIndex;
+
+        for (; slots[index] != null; index = (index + step) % size) {
+            if ((index + step) % size == hashIndex) {
+                index = hashIndex;
+                break;
+            }
+        }
+
+        slots[index] = key;
+        values[index] = value;
+
+    }
+
+    public T get(String key)
+    {
+        int hashIndex = hashFun(key);
+        int index = hashIndex;
+
+        for (; slots[index] != null; index = (index + step) % size) {
+            if (slots[index].equals(key)) {
+                return values[index];
+            }
+
+            if ((index + step) % size == hashIndex) {
+                break;
+            }
+        }
+
+        return null;
+    }
+}
+
