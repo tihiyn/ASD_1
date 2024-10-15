6c2d85b259ed0b23feb8dd747c69ca2bf110c94e
Nikita Tikhomirov <tihomnik@yandex.ru>
2024-10-15 21:54:49 +0300

    Divided logic hash-table and dyn array

--- Lesson_8/DynamicHashTable.java
+++ Lesson_8/DynamicHashTable.java
@@ -1,16 +1,16 @@
 import java.lang.reflect.Array;
 
 public class DynamicHashTable {
+    private final float loadFactor = 0.8f;
+    private final int step;
     public int size;
-    public int step;
-    public String[] slots;
+    private DynamicArray<String> slots;
 
     public DynamicHashTable(int sz, int stp)
     {
         size = sz;
         step = stp;
-        slots = new String[size];
-        for(int i=0; i<size; i++) slots[i] = null;
+        slots = new DynamicArray<>(String.class, size);
     }
 
     public int hashFun(String value)
@@ -29,7 +29,7 @@ public class DynamicHashTable {
         int hashIndex = hashFun(value);
         int index = hashIndex;
 
-        for (; slots[index] != null; index = (index + step) % size) {
+        for (; slots.getItem(index) != null; index = (index + step) % size) {
             if ((index + step) % size == hashIndex) {
                 return -1;
             }
@@ -40,14 +40,21 @@ public class DynamicHashTable {
 
     public int put(String value)
     {
+        if (slots.count == (int) (size * loadFactor)) {
+            String[] oldSlots = new String[size];
+            copy(oldSlots);
+            size *= 2;
+            slots.makeArray(size);
+            rehash(oldSlots);
+        }
+
         int slotIndex = seekSlot(value);
 
         if (slotIndex == -1) {
-            resize();
-            slotIndex = seekSlot(value);
+            return -1;
         }
 
-        slots[slotIndex] = value;
+        slots.set(value, slotIndex);
         return slotIndex;
     }
 
@@ -56,8 +63,8 @@ public class DynamicHashTable {
         int hashIndex = hashFun(value);
         int index = hashIndex;
 
-        for (; slots[index] != null; index = (index + step) % size) {
-            if (slots[index].equals(value)) {
+        for (; slots.getItem(index) != null; index = (index + step) % size) {
+            if (slots.getItem(index).equals(value)) {
                 return index;
             }
 
@@ -69,18 +76,82 @@ public class DynamicHashTable {
         return -1;
     }
 
-    private void resize() {
-        String[] oldArray = slots;
-        size *= 2;
-        slots = (String[]) Array.newInstance(String.class, size);
-
-        rehash(oldArray);
+    private void copy(String[] oldSlots) {
+        for (int i = 0; i < slots.capacity; i++) {
+            oldSlots[i] = slots.getItem(i);
+        }
     }
 
-    private void rehash(String[] array) {
-        for (String s: array) {
-            slots[seekSlot(s)] = s;
+    private void rehash(String[] oldSlots) {
+        for (int i = 0; i < oldSlots.length; i++) {
+            String oldSlot = oldSlots[i];
+            if (oldSlot != null) {
+                slots.remove(i);
+                slots.set(oldSlot, seekSlot(oldSlot));
+            }
         }
+    }
+}
+
+class DynamicArray<T>
+{
+    public T[] array;
+    public int count;
+    public int capacity;
+    public Class<T> clazz;
+
+    public DynamicArray(Class<T> clz, int initCapacity)
+    {
+        clazz = clz;
+
+        count = 0;
+        makeArray(initCapacity);
+    }
+
+    public void makeArray(int new_capacity)
+    {
+        T[] oldArray = array;
+        capacity = new_capacity;
+        array = (T[]) Array.newInstance(this.clazz, capacity);
+        for(int i = 0; i < new_capacity; i++) {
+            array[i] = null;
+        }
+
+        if (oldArray == null) {
+            return;
+        }
+        System.arraycopy(oldArray, 0, array, 0, count);
+    }
+
+    public T getItem(int index)
+    {
+        if (index < 0 || index >= capacity) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        return array[index];
+    }
+
+    public void set(T itm, int index) {
+        if (index < 0 || index >= capacity) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (array[index] == null) {
+            count++;
+        }
+
+        array[index] = itm;
+    }
+
+    public void remove(int index)
+    {
+        if (index < 0 || index >= capacity) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        count--;
+        array[index] = null;
     }
 }
 
